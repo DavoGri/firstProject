239d7d8069022a824db0cfe50f49911d616f712e
David <david.gri8.3sve@mail.ru>
2024-01-04 21:49:50 +0300

    фухххх

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+
+    public function store(Request $request)
+    {
+
+
+    }
+
+
+    public function update(Request $request, $id)
+    {
+
+
+    }
+
+
+    public function delete($id)
+    {
+
+    }
+
+
+    public function show($id)
+    {
+
+    }
+}
